ca943c013bc3bd41526ecae4e3ab71343216a82c
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-04-16 09:40:49 -0300

    Criacao de token de usuario

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,60 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // Usuario existe
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new ErrorEvent("Email or password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    // Senha está correta
+    if (!passwordMatch) {
+      throw new ErrorEvent("Email or password incorrect");
+    }
+
+    // Gerar jsonwebtoken
+    const token = sign({}, "335cd5e290807fd304c6b635e7cb0c5c", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
