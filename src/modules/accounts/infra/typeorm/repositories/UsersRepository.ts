23d3a7b6b054cdf143fb32f2d0a3b817c56048f9
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-04-29 11:44:21 -0300

    Refatoracao de repositorios

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { getRepository, Repository } from "typeorm";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
-import { User } from "../entities/User";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
 
 class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
