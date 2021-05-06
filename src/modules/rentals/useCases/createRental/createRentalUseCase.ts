eae61f07757be532bbc97e28c60c7f7be7ebd087
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-05-06 12:35:15 -0300

    Controller de registro de aluguel

--- src/modules/rentals/useCases/createRental/createRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/createRentalUseCase.ts
@@ -2,14 +2,28 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
 
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+@injectable()
 class CreateRentalUseCase {
   constructor(
+    @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
+    @inject("DayjsDateProvider")
     private dateProvider: IDateProvider,
   ) {}
 
-  async execute({ user_id, car_id, expected_return_date }): Promise<Rental> {
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
     const minimumRentalHours = 24;
 
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
