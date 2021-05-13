b5fe7411c8fed72d2d47a47604edf4a5251cb435
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-05-13 09:39:23 -0300

    Car devolution controller

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,12 +2,20 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/createRentalController";
 
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  devolutionRentalController.handle,
+);
 
 export { rentalRoutes };
