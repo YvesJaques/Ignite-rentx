6b71e035a0551ab65e9272a0b628d76f96cdffef
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-01 19:07:53 -0300

    Avatar access URL

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import multer from "multer";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import uploadConfig from "@config/upload";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
@@ -12,6 +13,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
 
@@ -21,5 +23,7 @@ usersRoutes.patch(
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle,
 );
+
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
